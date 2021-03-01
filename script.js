073909974381fc94045f7af1d4e1f58bdf18621b
Anniina Aarnio <aarnioanniina@gmail.com>
2021-03-01 21:03:26 +0200

    add buttons

--- script.js
+++ script.js
@@ -2,23 +2,38 @@ const backButton = document.getElementById("btnBackSignup");
 const signUpShow = document.getElementById("sign-in");
 const logInShow = document.getElementById("login");
 const buttons = document.getElementById("buttons");
+const logIn = document.getElementById("loginButton");
+const accountNav = document.getElementById("settings");
+const logOut = document.getElementById("btnLogout");
 
-signUpButton = () => {
-    hideButtons();
+
+
+showToDoList = signUpButton = () => {
+  hideButtons();
   signUpShow.classList.remove("hide");
 };
 goBack = () => {
   signUpShow.classList.add("hide");
   logInShow.classList.add("hide");
-  buttons.classList.remove('hide');
-
+  buttons.classList.remove("hide");
 };
 
 logInButton = () => {
-    hideButtons();
+  hideButtons();
   logInShow.classList.remove("hide");
 };
 
-hideButtons=() =>{
-    buttons.classList.add('hide');
+hideButtons = () => {
+  buttons.classList.add("hide");
+};
+
+loggedIn = () => {
+  hideButtons();
+  logInShow.classList.add("hide");
+  accountNav.classList.remove("hide");
+};
+logOutButton = () => {
+  logIn.classList.add('hide')
+  accountNav.classList.add('hide')
+  buttons.classList.remove("hide");
 }
